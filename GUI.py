5a02a4fcc1c97cef4c5a3de59b46c03f6ae72869
Tiannan Guo <tiannan.guo@gmail.com>
2016-06-30 22:32:31 +0200

    re designed the GUI

---
+++ GUI.py
@@ -0,0 +1,116 @@
+__author__ = 'Tiannan Guo, ETH Zurich 2016'
+from Tkinter import *
+from tkFileDialog import askopenfilenames, askopenfilename
+from tkintertable import TableCanvas, TableModel
+import tkFont
+
+
+def show_label_title(root, label_text, row_number, column_num):
+
+    Label(root, text=label_text, bg="white", fg="blue").grid(row=row_number, column=column_num, columnspan=3, sticky=W)
+
+def show_label(root, label_text, row_number, column_num):
+
+    Label(root, text=label_text, bg="white", fg="black").grid(row=row_number, column=column_num, padx=2, pady=2, sticky=W)
+
+
+def show_button(root, label_text, function_name, row_number, column_num):
+
+    Button(root, text=label_text, command=function_name).grid(row=row_number, column=column_num, padx=2, pady=2)
+
+def show_entry(root, default_value, row_number, column_num):
+
+    e_peak_RT = Entry(root, bg="white", fg="black", width=2)
+    e_peak_RT.insert(0, default_value)
+    e_peak_RT.grid(row=row_number, column=column_num, padx=2, pady=2, sticky=W)
+    return e_peak_RT
+
+
+
+# chrom file
+def chrom_readme():
+    win = Toplevel()
+    Label(win, text="combined chromatogram file").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+def chrom_browse():
+    global com_chrom_file_name
+    file = askopenfilename()
+    com_chrom_file_name = file
+
+def chrom_show():
+    #if 'com_chrom_file_name' in globals():
+        # show the first 10 lines of the file in a new window, preferable in a table
+    print "to do: show the first lines of ", com_chrom_file_name
+
+# norm file
+def norm_readme():
+    win = Toplevel()
+    Label(win, text="normalization file").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+def norm_browse():
+    global norm_file_name
+    file = askopenfilename()
+    norm_file_name = file
+
+def norm_show():
+    #if 'com_chrom_file_name' in globals():
+        # show the first 10 lines of the file in a new window, preferable in a table
+    print "to do: show the first lines of ", norm_file_name
+
+# sample file
+def sample_readme():
+    win = Toplevel()
+    Label(win, text="sample annotation file").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+def sample_browse():
+    global sample_file_name
+    file = askopenfilename()
+    sample_file_name = file
+
+def sample_show():
+    #if 'com_chrom_file_name' in globals():
+        # show the first 10 lines of the file in a new window, preferable in a table
+    print "to do: show the first lines of ", sample_file_name
+
+# pepID file
+def pepID_readme():
+    win = Toplevel()
+    Label(win, text="peptide identification file").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+def pepID_browse():
+    global pepID_file_name
+    file = askopenfilename()
+    pepID_file_name = file
+
+def pepID_show():
+    #if 'com_chrom_file_name' in globals():
+        # show the first 10 lines of the file in a new window, preferable in a table
+    print "to do: show the first lines of ", pepID_file_name
+
+
+# peak RT file
+def peakRT_readme():
+    win = Toplevel()
+    Label(win, text="peak group retention time (RT) tolerance").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+# bin RT file
+def binRT_readme():
+    win = Toplevel()
+    Label(win, text="binning retention time value tolerance").pack()
+    Button(win, text="close", command=win.destroy).pack()
+
+
+
+
+
+def run_expert():
+
+    print "run expert"
+
+def view_plots():
+    print "view plots"
--- GUI_entry.py
+++ GUI_entry.py
@@ -1,49 +1,8 @@
 from Tkinter import *
 from tkFileDialog import askopenfilenames, askopenfilename
+import tkFont
 from tkintertable import TableCanvas, TableModel
-
-def select_com_chrom_files(chrom_file_entry):
-    global com_chrom_file_name
-    file = askopenfilename()
-    com_chrom_file_name = file
-    chrom_file_entry.delete(0, END)
-    chrom_file_entry.insert(0, com_chrom_file_name)
-
-
-def select_library_file(library_file_entry):
-    global library_file_name
-    file = askopenfilename()
-    library_file_name = file
-    library_file_entry.delete(0, END)
-    library_file_entry.insert(0, library_file_name)
-
-def select_sample_file(sample_file_entry):
-    global sample_file_name
-    file = askopenfilename()
-    sample_file_name = file
-    sample_file_entry.delete(0, END)
-    sample_file_entry.insert(0, sample_file_name)
-
-def select_nf_file(nf_file_entry):
-    global nf_file_name
-    file = askopenfilename()
-    nf_file_name = file
-    nf_file_entry.delete(0, END)
-    nf_file_entry.insert(0, nf_file_name)
-
-def select_pepID_file(pepID_file_entry):
-    global pepID_file_name
-    file = askopenfilename()
-    pepID_file_name = file
-    pepID_file_entry.delete(0, END)
-    pepID_file_entry.insert(0, pepID_file_name)
-
-def run_expert():
-
-    print "run expert"
-
-def view_plots():
-    print "view plots"
+import GUI
 
 def main():
 
@@ -55,210 +14,132 @@ def main():
     row_number = 0
 
     # step 1
-    Label(root, text="Step 1. specify input files", bg="white", fg="black", relief=RIDGE).grid(
-        row=row_number)
+    GUI.show_label_title(root, "Step 1. input files", row_number, 0)
     row_number += 1
 
     # select com_chrom file
-    Label(root, text="select com_chrom file:", bg="white", fg="blue").grid(row=row_number, column=0, padx=2, pady=2)
-    chrom_file_button = Button(root, text='browse...', command=(lambda: select_com_chrom_files(chrom_file_entry)))
-    chrom_file_button.grid(row=row_number, column=1, padx=2, pady=2)
-    chrom_file_entry = Entry(root, bg="white", fg="black", width=20)
-    chrom_file_entry.insert(0, 'paste the directory and file name')
-    chrom_file_entry.grid(row=row_number, column=2, padx=2, pady=2)
+    GUI.show_label(root, "chrom:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.chrom_readme, row_number, 2)
+    GUI.show_button(root, "browse", GUI.chrom_browse, row_number, 3)
+    GUI.show_button(root, "show", GUI.chrom_show, row_number, 4)
+
+    # select normalization
+    GUI.show_label(root, "norm:", 1, 6)
+    GUI.show_button(root, "readme", GUI.norm_readme, row_number, 7)
+    GUI.show_button(root, "browse", GUI.norm_browse, row_number, 8)
+    GUI.show_button(root, "show", GUI.norm_show, row_number, 9)
+
     row_number += 1
 
     # select sample file
-    Label(root, text="select sample name file:", bg="white", fg="blue").grid(row=row_number, column=0,
-                                                                                              padx=2, pady=2)
-    sample_file_button = Button(root, text='browse...', command=(lambda: select_sample_file(sample_file_entry)))
-    sample_file_button.grid(row=row_number, column=1, padx=2, pady=2)
-    sample_file_entry = Entry(root, bg="white", fg="black", width=20)
-    sample_file_entry.insert(0, 'paste the directory and file name')
-    sample_file_entry.grid(row=row_number, column=2, padx=2, pady=2)
-    row_number += 1
+    GUI.show_label(root, "sample:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.chrom_readme, row_number, 2)
+    GUI.show_button(root, "browse", GUI.chrom_browse, row_number, 3)
+    GUI.show_button(root, "show", GUI.chrom_show, row_number, 4)
 
-    # select library file
-    Label(root, text="select library file:", bg="white", fg="blue").grid(row=row_number, column=0, padx=2, pady=2)
-    library_file_button = Button(root, text='browse...', command=(lambda: select_library_file(library_file_entry)))
-    library_file_button.grid(row=row_number, column=1, padx=2, pady=2)
-    library_file_entry = Entry(root, bg="white", fg="black", width=20)
-    library_file_entry.insert(0, 'paste the directory and file name')
-    library_file_entry.grid(row=row_number, column=2, padx=2, pady=2)
-    row_number += 1
+    # select peptide ID file
+    GUI.show_label(root, "pepID:", row_number, 6)
+    GUI.show_button(root, "readme", GUI.pepID_readme, row_number, 7)
+    GUI.show_button(root, "browse", GUI.pepID_browse, row_number, 8)
+    GUI.show_button(root, "show", GUI.pepID_show, row_number, 9)
 
-    # select normalization factor file
-    Label(root, text="select normalization factor file:", bg="white", fg="blue").grid(row=row_number, column=0, padx=2,
-                                                                                              pady=2)
-    nf_file_button = Button(root, text='browse...', command=(lambda: select_nf_file(nf_file_entry)))
-    nf_file_button.grid(row=row_number, column=1, padx=2, pady=2)
-    nf_file_entry = Entry(root, bg="white", fg="black", width=20)
-    nf_file_entry.insert(0, 'paste the directory and file name')
-    nf_file_entry.grid(row=row_number, column=2, padx=2, pady=2)
-    row_number += 1
-
-    # select peptide identification file
-    Label(root, text="select peptide identification files:", bg="white", fg="blue").grid(row=row_number,
-                                                                                                           column=0,
-                                                                                                           padx=2,
-                                                                                                           pady=2)
-
-    pepID_file_button = Button(root, text='browse...', command=(lambda: select_pepID_file(pepID_file_entry)))
-    pepID_file_button.grid(row=row_number, column=1, padx=2, pady=2)
-    pepID_file_entry = Entry(root, bg="white", fg="black", width=20)
-    pepID_file_entry.insert(0, 'paste the directory and file name')
-    pepID_file_entry.grid(row=row_number, column=2, padx=2, pady=2)
     row_number += 1
 
     # step 2
-    Label(root, text="", bg="white").grid(row=row_number)
-    row_number += 1
-    Label(root, text="Step 2. set parameters", bg="white", fg="black", relief=RIDGE).grid(
-        row=row_number)
+    GUI.show_label_title(root, "Step 2. parameters", row_number, 0)
     row_number += 1
 
-    #peak RT tolerance
-    Label(root, text="peak RT tolerance (s):", bg="white", fg="blue").grid(row=row_number,
-                                                                                    column=0, padx=2,pady=2)
-    peak_rt_tol_entry = Entry(root, bg="white", fg="black", width=4)
-    peak_rt_tol_entry.insert(0, '6')
-    peak_rt_tol_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # peak RT
+    GUI.show_label(root, "peak RT:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 2)
+    e_peak_RT = GUI.show_entry(root, 6, row_number, 3)
 
-    # MIN_FRAGMENTS
-    Label(root, text="min fragment number:", bg="white", fg="blue").grid(row=row_number, column=2,
-                                                                                               padx=2,
-                                                                                               pady=2)
-    min_fragment_entry = Entry(root, bg="white", fg="black", width=4)
-    min_fragment_entry.insert(0, '4')
-    min_fragment_entry.grid(row=row_number, column=3, padx=2, pady=2)
+    # bin RT
+    GUI.show_label(root, "bin RT:", row_number, 5)
+    GUI.show_button(root, "readme", GUI.binRT_readme, row_number, 6)
+    e_bin_RT = GUI.show_entry(root, 5, row_number, 7)
+
+    # min fragment 1
+    GUI.show_label(root, "min frag1:", row_number, 9)
+    GUI.show_button(root, "readme", GUI.binRT_readme, row_number, 10)   ##############re-write func
+    e_min_frag1 = GUI.show_entry(root, 5, row_number, 11)
+
     row_number += 1
 
-    #     MIN_FRAGMENTS_HIGHER_THAN_INPUT_NO_MS1
-    Label(root, text="MIN_FRAGMENTS_HIGHER_THAN_INPUT_NO_MS1:", bg="white", fg="blue").grid(row=row_number, column=0,
-                                                                                             padx=2,pady=2)
-    min_fragment_higher_than_input_no_ms1_entry = Entry(root, bg="white", fg="black", width=4)
-    min_fragment_higher_than_input_no_ms1_entry.insert(0, '4')
-    min_fragment_higher_than_input_no_ms1_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # min fragment 2
+    GUI.show_label(root, "min frag2:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 2) ##############re-write func
+    e_min_frag2 = GUI.show_entry(root, 6, row_number, 3)
 
-    #         MIN_FRAGMENTS_HIGHER_THAN_INPUT_UNIQUE_MS1
-    Label(root, text="MIN_FRAGMENTS_HIGHER_THAN_INPUT_UNIQUE_MS1:", bg="white", fg="blue").grid(row=row_number,
-                                                                                                                column=2,
-                                                                                                                padx=2,
-                                                                                                                pady=2)
-    min_fragment_higher_than_input_uni_ms1_entry = Entry(root, bg="white", fg="black", width=4)
-    min_fragment_higher_than_input_uni_ms1_entry.insert(0, '4')
-    min_fragment_higher_than_input_uni_ms1_entry.grid(row=row_number, column=3, padx=2, pady=2)
+    # min fragment 3
+    GUI.show_label(root, "min frag3:", row_number, 5)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 6) ##############re-write func
+    e_min_frag3 = GUI.show_entry(root, 6, row_number, 7)
+
+
+    # peak shape 1
+    GUI.show_label(root, "peak shape 1:", row_number, 9)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 10) ##############re-write func
+    e_peak_shape1 = GUI.show_entry(root, 6, row_number, 11)
+
     row_number += 1
 
-    #             MAX_PEAK_WIDTH
-    Label(root, text="MAX_PEAK_WIDTH:", bg="white", fg="blue").grid(
-        row=row_number,column=0,padx=2,pady=2)
-    max_peak_width_entry = Entry(root, bg="white", fg="black", width=4)
-    max_peak_width_entry.insert(0, '4')
-    max_peak_width_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # peak shape 2
+    GUI.show_label(root, "peak shape 2:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 2) ##############re-write func
+    e_peak_shape2 = GUI.show_entry(root, 6, row_number, 3)
 
-    #                 PEAK_WIDTH_FOLD_VARIATION
-    Label(root, text="PEAK_WIDTH_FOLD_VARIATION:", bg="white", fg="blue").grid(
-        row=row_number, column=2, padx=2, pady=2)
-    peak_width_tol_entry = Entry(root, bg="white", fg="black", width=4)
-    peak_width_tol_entry.insert(0, '2.0')
-    peak_width_tol_entry.grid(row=row_number, column=3, padx=2, pady=2)
+    # peak shape 3
+    GUI.show_label(root, "peak shape 3:", row_number, 5)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 6) ##############re-write func
+    e_peak_shape3 = GUI.show_entry(root, 6, row_number, 7)
+
+    # peak shape 4
+    GUI.show_label(root, "peak shape 4:", row_number, 9)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 10) ##############re-write func
+    e_peak_shape4 = GUI.show_entry(root, 6, row_number, 11)
+
     row_number += 1
 
-    #                     PEAK_SHAPE_FOLD_VARIATION
-    Label(root, text="PEAK_SHAPE_FOLD_VARIATION:", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-    peak_shape_tol_entry = Entry(root, bg="white", fg="black", width=4)
-    peak_shape_tol_entry.insert(0, '2.5')
-    peak_shape_tol_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # output file
+    GUI.show_label(root, "output good:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 2) ##############re-write func
+    e_output_good = Entry(root, bg="white", fg="black", width=20)
+    e_output_good.insert(0, "output_good_tg.txt")
+    e_output_good.grid(row=row_number, column=3, padx=2, pady=2, sticky=W, columnspan=5)
 
-    #                         PEAK_SHAPE_FOLD_VARIATION_CRUDE
-    Label(root, text="PEAK_SHAPE_FOLD_VARIATION_CRUDE:", bg="white", fg="blue").grid(
-        row=row_number, column=2, padx=2, pady=2)
-    peak_shape_tol_crude_entry = Entry(root, bg="white", fg="black", width=4)
-    peak_shape_tol_crude_entry.insert(0, '2.0')
-    peak_shape_tol_crude_entry.grid(row=row_number, column=3, padx=2, pady=2)
     row_number += 1
 
-    #                             BINNING_RT_VALUE_TOLERANCE
-    Label(root, text="BINNING_RT_VALUE_TOLERANCE:", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-    binning_rt_tol_entry = Entry(root, bg="white", fg="black", width=4)
-    binning_rt_tol_entry.insert(0, '5')
-    binning_rt_tol_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # step 3 output file
+    GUI.show_label_title(root, "Step 3. run the expert system", row_number, 0)
 
-    #                                 PEAK_BOUNDARY_RT_LEFT_RIGHT_RATIO_TOLERANCE
-    Label(root, text="PEAK_BOUNDARY_RT_LEFT_RIGHT_RATIO_TOLERANCE:", bg="white", fg="blue").grid(
-        row=row_number, column=2, padx=2, pady=2)
-    peak_boundary_ratio_tol_entry = Entry(root, bg="white", fg="black", width=4)
-    peak_boundary_ratio_tol_entry.insert(0, '0.6')
-    peak_boundary_ratio_tol_entry.grid(row=row_number, column=3, padx=2, pady=2)
     row_number += 1
 
-    # step 3
-    Label(root, text="", bg="white").grid(row=row_number)
-    row_number += 1
-    Label(root, text="Step 3. Run DIA expert", bg="white", fg="black", relief=RIDGE).grid(
-        row=row_number)
+    Button(root, text="Click to Run the Expert System", command=GUI.peakRT_readme).grid(row=row_number, column=1, padx=2, pady=2, columnspan=3)  ##############re-write func
+
     row_number += 1
 
-    # output_chrom_file_name
-    Label(root, text="output_chrom_file_name:", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-    output_chrom_file_entry = Entry(root, bg="white", fg="black", width=20)
-    output_chrom_file_entry.insert(0, 'type in the file name')
-    output_chrom_file_entry.grid(row=row_number, column=1, padx=2, pady=2)
+    # step 4 view refined plot
+    GUI.show_label_title(root, "Step 4. view plots", row_number, 0)
+
     row_number += 1
 
-    # not_selected_tg
-    Label(root, text="not_selected_tg_file_name:", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-    not_selected_tg_file_entry = Entry(root, bg="white", fg="black", width=20)
-    not_selected_tg_file_entry.insert(0, 'type in the file name')
-    not_selected_tg_file_entry.grid(row=row_number, column=1, padx=2, pady=2)
-    row_number += 1
-    # run the expert program
-    run_expert_button = Button(root, text="run expert", command=run_expert).grid(row=row_number, column=0)
-    row_number += 1
+    GUI.show_label(root, "peptide:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.norm_readme, row_number, 2)  ##############re-write func
+    GUI.show_button(root, "browse", GUI.norm_browse, row_number, 3)  ##############re-write func
+    GUI.show_button(root, "show", GUI.norm_show, row_number, 4)  ##############re-write func
 
-    # step 4
-    Label(root, text="", bg="white").grid(row=row_number)
-    row_number += 1
-    Label(root, text="Step 4. View plots", bg="white", fg="black", relief=RIDGE).grid(
-        row=row_number)
+    GUI.show_label(root, "samples:", row_number, 6)
+    GUI.show_button(root, "readme", GUI.norm_readme, row_number, 7)  ##############re-write func
+    GUI.show_button(root, "browse", GUI.norm_browse, row_number, 8)  ##############re-write func
+    GUI.show_button(root, "show", GUI.norm_show, row_number, 9)  ##############re-write func
+
     row_number += 1
 
-    # list of tg
-    Label(root, text="list of tg", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-
-    selected_tg_entry = Entry(root, bg="white", fg="black", width=20)
-    selected_tg_entry.insert(0, 'type in the file name')
-    selected_tg_entry.grid(row=row_number, column=1, padx=2, pady=2)
-    row_number += 1
-
-    # list of samples
-    Label(root, text="list of samples", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-
-    selected_samples_text = Text(root, height=10, bg="white", fg="black", width=20)
-    selected_samples_text.insert(INSERT, 'put sample names, each sample per line')
-    selected_samples_text.grid(row=row_number, column=1, padx=2, pady=2)
-    row_number += 1
-
-    # range of RT
-    Label(root, text="range of RT", bg="white", fg="blue").grid(
-        row=row_number, column=0, padx=2, pady=2)
-    view_rt_range_entry = Entry(root, bg="white", fg="black", width=20)
-    view_rt_range_entry.insert(0, 'type in the file name')
-    view_rt_range_entry.grid(row=row_number, column=1, padx=2, pady=2)
-    row_number += 1
-
-    # run the expert program
-    view_plots_button = Button(root, text="view plots", command=view_plots).grid(row=row_number, column=0)
-    row_number += 1
-    Label(root, text="", bg="white").grid(row=row_number)
-
+    # peak shape 2
+    GUI.show_label(root, "RT range:", row_number, 1)
+    GUI.show_button(root, "readme", GUI.peakRT_readme, row_number, 2) ##############re-write func
+    e_view_rt_range = GUI.show_entry(root, 6, row_number, 3)
 
     root.mainloop()
 
